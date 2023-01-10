1a4b96afdd40b47a9536eda9d59cee1ccff77fdf
Chandan6169 <106052207+Chandan6169@users.noreply.github.com>
2023-01-10 02:58:04 +0000

    calculator is ready

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 function clearScreen(){
-    document.getElementById('demo').value='';
+    document.getElementById('result').value='';
+}
+function buttonClick(a){
+    document.getElementById('result').value +=a;
+}
+function switchTheme(){
+    let darkMode = document.getElementById('dark-mode');
+    let theme = document.getElementById("theme");
+
+    if(theme.getAttribute('href')=='light.css'){
+        theme.href = "dark.css";
+        darkMode.innerHTML='Light Mode';
+    }else{
+        theme.href = "light.css";
+        darkMode.innerHTML='Dark Mode';
+    }
 }
